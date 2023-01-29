b8ceebbdd7898830f2d77179c1350b8adaa19b04
Samgesah <samgesa@yahoo.com>
2023-01-29 21:52:57 +0300

    update

--- print_base.c
+++ print_base.c
@@ -48,6 +48,7 @@ int print_hex_big(va_list variables, flags_t *f)
  * @variables: va_list arguments from _printf
  * Description: the function calls convert() which in turns converts the input
  * number into the correct base and returns it as a string
+ *@f: pointer
  * Return: the number of char printed
  */
 int print_binary(va_list variables, flags_t *f)
@@ -73,6 +74,7 @@ int print_octal(va_list variables, flags_t *f)
 	unsigned int num = va_arg(variables, unsigned int);
 	char *str = convert(num, 8, 0);
 	int count = 0;
+
 	if (f->hash == 1 && str[0] != '0')
 		count += _putchar('0');
 	count += _puts(str);
