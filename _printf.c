f59a341575c2e81a6ef84fa33119c41cbd460c76
Samgesah <samgesa@yahoo.com>
2023-01-29 18:33:38 +0300

    a function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -1,44 +1,25 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * _printf - Printf function
- * @format: format.
- * Return: character string
- */
+  *_printf - function my printf
+  *@format: string with format to print
+  *
+  *Return: number of characters
+  */
+
 int _printf(const char *format, ...)
 {
-	flags_t flags = {0, 0, 0};
-	int count = 0;
-	int (*f)(va_list,  flags_t *);
-	va_list variables;
-	const char *L;
+	va_list args;
 
-	va_start(variables, format);
-	/*prevent passing a null pointer*/
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (L = format; *L; L++)
-	{
-		if (*L == '%')
-		{
-			L++;
-			if (*L == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (check_flag(*L, &flags))
-				L++;
-			f = check_specifier(*L);
-			count += (f)
-				? f(variables, &flags)
-				: _printf("%%%c", *L);
-		} else
-			count += _putchar(*L);
-	}
-	_putchar(-1);
-	va_end(variables);
-	return (count);
+	int length = 0;
+
+	if (format == NULL)
+		return (1);
+
+	va_start(args, format);
+	length = _print_format(format, args);
+	va_end(args);
+
+	return (length);
 }
-
